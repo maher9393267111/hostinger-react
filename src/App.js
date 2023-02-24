f78cd5929609f29f4dec174f869c060a5a74d3dc
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-02-25 00:04:41 +0300

    add

--- src/App.js
+++ src/App.js
@@ -3,22 +3,95 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+<div>
+
+
+<div className="col-lg-7 mt-5 mt-lg-0">
+                    <div id="accordion" className="accordion-area">
+                        <div className="card single-faq-inner">
+                            <div className="card-header" id="ff-one">
+                                <h5 className="mb-0">
+                                    <button className="btn-link" data-toggle="collapse" data-target="#f-one" aria-expanded="true" aria-controls="f-one">
+                                        01.  What does you simply dummy in do ?
+                                        <i className="fa fa-eye"></i>
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="f-one" className="show collapse" aria-labelledby="ff-one" data-parent="#accordion">
+                                <div className="card-body">
+                                    What does you dummy text of free available in market printing has industry been industry's standard dummy text ever.
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card single-faq-inner">
+                            <div className="card-header" id="ff-two">
+                                <h5 className="mb-0">
+                                    <button className="btn-link collapsed" data-toggle="collapse" data-target="#f-two" aria-expanded="true" aria-controls="f-two">
+                                        02.  What graphics dummy of free  design ?
+                                        <i className="fa fa-eye"></i>
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="f-two" className="collapse" aria-labelledby="ff-two" data-parent="#accordion">
+                                <div className="card-body">
+                                    What graphics simply dummy text of free available in market printing industry has been industry's standard dummy text ever.
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card single-faq-inner">
+                            <div className="card-header" id="ff-three">
+                                <h5 className="mb-0">
+                                    <button className="btn-link collapsed" data-toggle="collapse" data-target="#f-three" aria-expanded="true" aria-controls="f-three">
+                                        03.  Why we are the best ?
+                                        <i className="fa fa-eye"></i>
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="f-three" className="collapse" aria-labelledby="ff-three" data-parent="#accordion">
+                                <div className="card-body">
+                                    Why we are dummy text of free available in market printing industry has been industry's standard dummy text ever.
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card single-faq-inner">
+                            <div className="card-header" id="ff-four">
+                                <h5 className="mb-0">
+                                    <button className="btn-link collapsed" data-toggle="collapse" data-target="#f-four" aria-expanded="true" aria-controls="f-four">
+                                        04.  What industries dummy covered ?
+                                        <i className="fa fa-eye"></i>
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="f-four" className="collapse" aria-labelledby="ff-four" data-parent="#accordion">
+                                <div className="card-body">
+                                    What industries text of free available in market printing industry has been industry's standard dummy text ever.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+</div>
+
+
+
+
+
+		
+			
+
+
+
+
+
+
+    
   );
 }
 
